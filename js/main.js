58ffa3cb0dcff73026ff73065c613c7add03bd5f
aacox329 <acox@hubspot.com>
2017-10-23 13:07:38 -0400

    latest update

--- js/main.js
+++ js/main.js
@@ -1,15 +1,109 @@
+var counter = 0
+
+// poses contains a list of pose Objects
+// Each pose object contains
+// - Pose Name
+// - image
+// - sequence number
+
+var imageList = ['boat',
+'bridge',
+'cat',
+'chair',
+'chaturanga',
+'childspose',
+'cobra',
+'corpse',
+'cow',
+'crow',
+'dancer',
+'dolphin',
+'downdog',
+'eagle',
+'firelog',
+'fish',
+'floorbow',
+'forwardfold',
+'halfmoon',
+'halfsplits',
+'handstand',
+'happybaby',
+'lowlunge',
+'malasana',
+'plank-guide',
+'plow',
+'reversewarrior',
+'seatedforwardfold',
+'standingsplits',
+'tree',
+'warriorI',
+'warriorII',
+'warriorIII'];
 
 //callbacks//
 function allowDrop(ev){
-    ev.preventDefualt();
+    ev.preventDefault();
 }; //prevents opening a new page when you drag and element//
 
 function drag(ev){
     ev.dataTransfer.setData("text", ev.target.id);
 };
 
+//move images to right section
 function drop(ev){
-    ev.preventDefualt();
-    var data= ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElememntById(data));
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    var node = ev.target; // Need to get the actual destination node incase user drops on a child of that node
+    // while(!node.classList.contains("dst")){node=node.parentElement;}
+    if (ev.target.classList.contains("dst")) {
+        node.appendChild(document.getElementById(data));
+    } else {
+        console.log('starting the bubble up.');
+        console.log(node);
+        console.log(node.classList);
+        while(!node.classList.contains("pose")) {
+            node = node.parentElement;
+            console.log('reasign to the parent');
+            console.log(node);
+            console.log(node.classList);
+        }
+        $(document.getElementById(data)).insertBefore(node);
+    }
 }; //data should be the id of whatever we click on (the div)//
+
+//search for a pose image
+function search(ev){
+    var searchItem= $(this)[0].value;
+    if(imageList.includes(searchItem)){
+        var source= "images/StickSanEng/"+searchItem+".jpg";
+        $("#base").append("<div class='pose' id='"+String(counter)+"' draggable='true' ondragstart='drag(event)'><p>"+String(counter)+"</p><img src='"+source+"'></div>");
+        counter++;
+        $("#noImg").slideUp();
+    } else {
+        $("#noImg").slideDown();
+    }
+};
+
+//add a new section of the sequence - the .seq div
+//add h2 to .seq div
+//add input text to h2
+function section(ev){
+    var sectionName = $(this)[0].value;
+    console.log(sectionName);
+        $ ('<div class="seq dst" ondrop="drop(event)" ondragover="allowDrop(event)"><h2>' + sectionName + '</h2></div>').appendTo("body");
+//reset input to have no text
+    $(this)[0].value = "";
+
+};
+
+$(document).ready(function (){
+    var searchResult = $("#search");
+    if (searchResult && searchResult[0]) {
+        searchResult[0].addEventListener("change", search);
+    }
+    var addSection = $("#section");
+    if (addSection && addSection[0]) {
+        addSection[0].addEventListener("change", section);
+    }
+}
+);
